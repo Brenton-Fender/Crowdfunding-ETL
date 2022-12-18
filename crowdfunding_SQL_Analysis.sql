3a4ec8d43619d4c4d9722cf8e17c2d066fdc8b19
Brenton-Fender <105960365+Brenton-Fender@users.noreply.github.com>
2022-12-18 03:48:39 -0500

    Completed deliverable 4

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -1,19 +1,26 @@
 -- Challenge Bonus queries.
 -- 1. (2.5 pts)
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
-
+select backers_count, cf_id, outcome from campaign
+where outcome = 'live'
+order by backers_count desc
 
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
-
+select count(backer_id), cf_id from backers
+group by cf_id
+order by count(backer_id) desc
 
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
-
-
+select co.first_name, co.last_name, co.email, ca.goal - ca.pledged as goal_amount_remaining, ca.outcome 
+from campaign as ca
+inner join contacts as co on ca.contact_id = co.contact_id
+where outcome = 'live'
+order by goal_amount_remaining desc
 
 
 -- Check the table
@@ -23,9 +30,11 @@
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
-
-
-
+select co.email, co.first_name, co.last_name, ca.cf_id, ca.company_name, ca.description, ca.end_date, ca.goal - ca.pledged as "Left of Goal"
+into email_backers_remaining_goal_amount
+from campaign as ca
+inner join contacts as co on ca.contact_id = co.contact_id
+order by email desc
 -- Check the table
 
 
--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -49,6 +49,17 @@ CREATE TABLE "contacts" (
      )
 );
 
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
 ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
 REFERENCES "campaign" ("category_id");
 
@@ -57,4 +68,7 @@ REFERENCES "campaign" ("subcategory_id");
 
 ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "campaign" ("contact_id");
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
 
